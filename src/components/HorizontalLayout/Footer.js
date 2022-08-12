7659d5d56a20daf92fef73a73cbf1664b4ddf317
Dineshdk555 <104187302+Dineshdk555@users.noreply.github.com>
2022-08-12 11:06:00 +0530

    Update Footer.js

--- src/components/HorizontalLayout/Footer.js
+++ src/components/HorizontalLayout/Footer.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Container, Row, Col } from "reactstrap"
+import { Container, Row, Col, Button } from "reactstrap"
+import { Link } from 'react-router-dom'
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props"
 // import TwitterLogin from "react-twitter-auth"
 import { GoogleLogin } from "react-google-login"
@@ -13,33 +14,48 @@ const Footer = () => {
             <div className="col-md-3 col-sm-10">
               <ul className="list-unstyled ">
                 <h3>About</h3>
+                <Link 
+                role = "button"
+                to = "#" >
                 <li className="mt-3 ">About Us</li>
                 <li className="mt-3 ">How it works</li>
                 <li className="mt-3 ">Security</li>
+                </Link>
               </ul>
             </div>
 
             <div className="col-md-3 col-sm-6">
               <ul className="list-unstyled ">
                 <h3>Solution</h3>
+                <Link 
+                role = "button"
+                to = "#" >
                 <li className="mt-3 ">Enterprise</li>
                 <li className="mt-3 ">Private Label</li>
                 <li className="mt-3 ">Management</li>
+                </Link>
               </ul>
             </div>
 
             <div className="col-md-3 col-sm-6">
               <ul className="list-unstyled  ">
                 <h3>Contact</h3>
+                <Link 
+                role = "button"
+                to = "#" >
                 <li className="mt-3 ">Contact Us </li>
                 <li className="mt-3">Careers</li>
                 <li className="mt-3 ">Security</li>
+                </Link>
               </ul>
             </div>
 
             <div className="col-md-3 col-sm-6 ">
               <ul className="list-unstyled  ">
                 <h3>Follow Us</h3>
+                <Link 
+                role = "button"
+                to = "#" >
                 <li className="mt-3 ">
                   <i className="mdi mdi-facebook" />
                   Facebook{" "}
@@ -50,6 +66,7 @@ const Footer = () => {
                   <i className="mdi mdi-google" />
                   Google
                 </li>
+                </Link>
               </ul>
             </div>
 
