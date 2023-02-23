141f7ed512e8acd69694c8ceafa6b4dd46495603
kishorekmr30 <121477456+kishorekmr30@users.noreply.github.com>
2023-02-23 19:22:13 +0530

    projectdetail ui

--- src/pages/Projects/ProjectOverview/projectDetail.js
+++ src/pages/Projects/ProjectOverview/projectDetail.js
@@ -73,8 +73,10 @@ const ProjectDetail = ({ project }) => {
   }
 
   return (
-    <Card>
-      <CardBody id="projcard">
+    <Card id="projcard">
+      <div id="appcard">
+      <CardBody >
+        
           <div className="d-flex">
             <img
               src={project.img ? project.img : attImages[imgIndex].url}
@@ -340,8 +342,9 @@ const ProjectDetail = ({ project }) => {
             </div>
           </Col> */}
           </Row>
+         
       </CardBody>
-      
+      </div> 
     </Card>
   )
 }
