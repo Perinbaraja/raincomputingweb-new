d96f71054c01b70909b1f3e8374254fd50a27752
gokulgk2000 <110904612+gokulgk2000@users.noreply.github.com>
2023-03-14 00:26:02 +0530

    create remainder modal update

--- src/rainComputing/components/chat/ChatRemainder.js
+++ src/rainComputing/components/chat/ChatRemainder.js
@@ -1,23 +1,36 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Row } from "reactstrap"
 import PropTypes from "prop-types"
 import { createReminder } from "rainComputing/helpers/backend_helper"
 import toastr from "toastr"
 import "toastr/build/toastr.min.css"
 import { useChat } from "rainComputing/contextProviders/ChatProvider"
+import { useUser } from "rainComputing/contextProviders/UserProvider"
 
 const ChatRemainder = ({ setModalOpen, curMessageId }) => {
-  const { currentRoom: currentChat,setMessages,messages } = useChat()
+  const { currentRoom: currentChat, setMessages, messages } = useChat()
+  const { currentUser } = useUser()
 
   const [title, setTitle] = useState("")
   const [date, setDate] = useState("")
   const [time, setTime] = useState("")
-
+  const [userId, setUserId] = useState(null)
+  const [isChecked, setIsChecked] = useState("");
   toastr.options = {
     progressBar: true,
     closeButton: true,
   }
-
+  useEffect(() => {
+    if (isChecked) {
+      setUserId(currentUser?.userID);
+    } else {
+      setUserId("");
+    }
+  }, [isChecked, currentUser]);
+  
+  const handleChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
   const handlereminderCancel = () => {
     setModalOpen(false)
   }
@@ -28,10 +41,15 @@ const ChatRemainder = ({ setModalOpen, curMessageId }) => {
       title: title,
       date: date,
       time: time,
+    };
+    
+    if (isChecked) {
+      payload.userId = currentUser?.userID;
     }
     const reminderData = await createReminder(payload)
+    console.log("payload", payload)
     if (reminderData.success) {
-      console.log("data",reminderData)
+      console.log("data", reminderData)
       toastr.success("Reminder Create success")
       setModalOpen(false)
     } else {
@@ -39,6 +57,8 @@ const ChatRemainder = ({ setModalOpen, curMessageId }) => {
       setModalOpen(false)
     }
   }
+
+  console.log("userId",userId)
   return (
     <>
       <Row>
@@ -89,12 +109,33 @@ const ChatRemainder = ({ setModalOpen, curMessageId }) => {
             className="form-control"
             type="time"
             placeholder="HH:MM"
-            value= {time}
+            value={time}
             name="time"
             onChange={e => setTime(e.target.value)}
           />
         </div>
       </Row>
+      <Row className="my-md-3">
+  <div className=" d-flex pt-2">
+    <div className="form-check pl-4">
+      <input
+        className="form-check-input "
+        type="checkbox"
+        id="flexCheckDefault"
+        value={userId}
+        onChange={handleChange}
+        checked={isChecked}
+      />
+      <label
+        className="form-check-label ms-2"
+        htmlFor="flexCheckDefault"
+      >
+        Create a Self Reminder
+      </label>
+    </div>
+  </div>
+</Row>
+
       <Row>
         <div className="modal-footer">
           <button
