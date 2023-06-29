66e7a7175ff988b4d7f9b222232ac6dc3b1d7ca6
Srikanthk-2000 <srikanthrider8104@gmail.com>
2023-06-29 11:43:55 +0530

    pinmodel update

--- src/rainComputing/components/chat/models/PinnedModels.js
+++ src/rainComputing/components/chat/models/PinnedModels.js
@@ -10,11 +10,16 @@ import moment from "moment"
 import { useUser } from "rainComputing/contextProviders/UserProvider"
 import AttachmentViewer from "../AttachmentViewer"
 import PropTypes from "prop-types"
+import { DivIcon } from "leaflet"
+import { log } from "logrocket"
+import avatar from "assets/images/avatar-defult.jpg"
 
 const PinnedModels = ({ handleLocateMessage }) => {
   const { currentRoom: currentChat, setMessages, messages } = useChat()
   const [pinModal, setPinModal] = useState(false)
   const [pinnedMsg, setPinnedMsg] = useState([])
+  
+
   const handleClick = messageId => {
     handleLocateMessage(messageId)
     setPinModal(false)
@@ -73,7 +78,7 @@ const PinnedModels = ({ handleLocateMessage }) => {
         toggle={() => {
           tog_scroll()
         }}
-        // scrollable={true}
+      // scrollable={true}
       >
         <div className="modal-header ">
           <h5 className="modal-title mt-0">
@@ -90,93 +95,87 @@ const PinnedModels = ({ handleLocateMessage }) => {
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div className="d-flex justify-content-center">
-          <div className="w-75 ">
-            {pinnedMsg &&
-              pinnedMsg?.map((msg, m) => (
-                <div className=" border border-primary my-2 " key={m}>
-                  <div className="conversation-list">
-                    <div
-                      className="d-flex  justify-content-end"
-                      style={{
-                        backgroundColor: currentChat?.color
-                          ? currentChat?.color + "33"
-                          : "#00EE00" + "33",
-                      }}
-                    >
-                      <i
-                        className="mdi mdi-pin-off-outline mdi-rotate-315 text-danger"
-                        title="Unpin"
-                        onClick={() => handleUnpinMessage(msg?._id)}
-                        style={{ cursor: "pointer" }}
-                      ></i>
-                    </div>
-                    <div
-                      className="text-wrap   px-2 py-4"
-                      style={{
-                        backgroundColor: currentChat?.color
-                          ? currentChat?.color + "33"
-                          : "#00EE00" + "33",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => handleClick(msg._id)}
-                    >
-                      <div className="conversation-name">
-                        <div className="d-flex justify-content-between align-items-center">
-                          <div>
-                            <p className="text pt-1" style={text}>
-                              {getSender(msg)?.firstname}{" "}
-                              {getSender(msg)?.lastname}
-                            </p>
-                          </div>
-                          <div></div>
+        <div className="card">
+          <div className="d-flex justify-content-center py-3" style={{ backgroundColor: "#ededed" }}>
+            <div className="w-75" style={{ backgroundColor: "#f0f1f2" }}>
+              {pinnedMsg &&
+                pinnedMsg?.map((msg, m) => (
+                  <div className="" key={m}>
+                    <div className="card border-secondary border border-1 rounded-5" style={{ borderRadius: "5px" }}>
+
+                      <div className="d-flex justify-content-between px-2" style={{ backgroundcolor: "#a4a7ab" }}>
+                        <div className="">
+                          {/* <i class="bi bi-person-circle fs-1" style={{ color: "#3987ed" }}></i> */}
+                          <p className="rouded-circle text-primary text pt-1 ">
+                            <img className="avatar-sm me-2 rounded-circle " src={getSender(msg)?.profilePic ?  getSender(msg)?.profilePic
+                                : avatar}></img>
+                            {getSender(msg)?.firstname} {getSender(msg)?.lastname}
+                          </p>
+                        </div>
+                        <div className="">
+                          <i
+                            className="bi bi-x-lg bi-rotate-315 text-danger"
+                            title="Unpin"
+                            onClick={() => handleUnpinMessage(msg?._id)}
+                            style={{ cursor: "pointer" }}
+                          ></i>
                         </div>
                       </div>
+                      <div className="text-wrap px-2 py-2 " onClick={() => handleClick(msg._id)}>
+                        <div className="conversation-name">
+                          <div className="d-flex justify-content-between align-items-center">
+                            <div>{/* <p className="text pt-1" style={text}>{getSender(msg)?.firstname} {getSender(msg)?.lastname}</p> */}</div>
+                          </div>
+                        </div>
+                        <div className=" border border-success p-2 mb-2 border-opacity-25  rounded-2" style={{ borderColor: "#dbdbdb" }}>
+                          {msg.isAttachment ? (
+                            <>
+                              <AttachmentViewer attachments={msg.attachments} />
+                              <p>
+                                <div
+                                  style={{
+                                    wordBreak: "break-word fs-1S mx-2",
+                                    overflowWrap: "break-word",
+                                    whiteSpace: "pre-line",
+                                    height: "50px"
 
-                      <div className="mb-1">
-                        {msg.isAttachment ? (
-                          <>
-                            <AttachmentViewer attachments={msg.attachments} />
-                            <p>
+                                  }}
+                                  dangerouslySetInnerHTML={{
+                                    __html: msg?.messageData,
+                                  }}
+                                />
+                              </p>
+                            </>
+                          ) : (
                             <div
-                          style={{
-                            wordBreak: "break-word",
-                            overflowWrap: "break-word",
-                            whiteSpace: "pre-line",
-                          }}
-                          dangerouslySetInnerHTML={{
-                            __html: msg?.messageData,
-                          }}
-                        />
-                            </p>
-                            <div className="mt-1"></div>
-                          </>
-                        ) : (
-                          <div
-                          style={{
-                            wordBreak: "break-word",
-                            overflowWrap: "break-word",
-                            whiteSpace: "pre-line",
-                          }}
-                          dangerouslySetInnerHTML={{
-                            __html: msg?.messageData,
-                          }}
-                        />
-                        
-                        )}
+                              style={{
+                                wordBreak: "break-word",
+                                overflowWrap: "break-word",
+                                whiteSpace: "pre-line",
+                                height: "50px"
+                              }}
+                              dangerouslySetInnerHTML={{
+                                __html: msg?.messageData,
+                              }}
+                            />
+                          )}
+                        </div>
+                        <p className="chat-time">
+                          <i className="bx bx-comment-check align-middle" />
+                          <i class="bi bi-alarm aliiddlgn-me me-2" />
+                          {/* <i className="bx bx-time-five aliiddlgn-me me-1" /> */}
+                          {moment(msg.createdAt).format("DD-MM-YY HH:mm")}
+                        </p>
                       </div>
 
-                      <p className="chat-time mb-0 ">
-                        <i className="bx bx-comment-check align-middle " />
-                        {/* <i className="bx bx-time-five align-middle me-1" /> */}
-                        {moment(msg.createdAt).format("DD-MM-YY HH:mm")}
-                      </p>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+
+            </div>
           </div>
         </div>
+
       </Modal>
       <Dropdown isOpen={pinModal} toggle={tog_scroll}>
         <DropdownToggle className="btn nav-btn" tag="i">
